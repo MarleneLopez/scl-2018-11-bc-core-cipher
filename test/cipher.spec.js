c25b37e7554663e38068875cf738b76d51aac5b3
MarleneLopez <lopez.mpln@gmail.com>
2018-12-05 11:49:00 -0300

    actualizacion 1

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -4,11 +4,16 @@ describe('cipher', () => {
     assert.equal(typeof cipher, 'object');
   });
 
+
+
   describe('cipher.encode', () => {
 
     it('debería ser una función', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
+
+
+
 
     it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33');
   });
@@ -18,6 +23,9 @@ describe('cipher', () => {
     it('debería ser una función', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
+
+
+
 
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33');
   });
