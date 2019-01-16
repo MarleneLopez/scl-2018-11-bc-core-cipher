3208b2c08b5278fc43289494e8c5f6cc9a5a411d
MarleneLopez <lopez.mpln@gmail.com>
2019-01-16 17:29:35 -0300

    Initial src subtree commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-window.onload = mostrar; 
+window.onload = mostrar;
 
-function mostrar(){
-  document.getElementById("root").style.display="block";
-  document.getElementById("cesar").style.display="none";
-  document.getElementById("parrafoTextoEntregado").style.display="none";
+function mostrar() {
+  document.getElementById("root").style.display = "block";
+  document.getElementById("cesar").style.display = "none";
+  document.getElementById("parrafoTextoEntregado").style.display = "none";
 
 }
 
@@ -11,47 +11,47 @@ function mostrar(){
 //Se reemplazó el onclick() por addEventListener, para eso se creó un id donde
 //habia onclick(). Ya no es una función (function)!!
 
-  document.getElementById("comenzar").addEventListener('click', (evento) => {
-     evento.preventDefault();
-  document.getElementById("root").style.display="none";
-  document.getElementById("cesar").style.display="block";
+document.getElementById("comenzar").addEventListener('click', (evento) => {
+  evento.preventDefault();
+  document.getElementById("root").style.display = "none";
+  document.getElementById("cesar").style.display = "block";
 })
 
 //Cifrando
-  document.getElementById("cifrar").addEventListener('click', (evento) => {
-   evento.preventDefault();
-//guardar valores de los imput
-      let texto = document.getElementById("inputTexto").value;
-      let offset = document.getElementById("inputOffset").value;
-//guardar en resultado todo el contenido de cipher
-      let resultadoCodificar = cipher.encode(texto, offset);
-//reemplazar mi parrafo vacio por resultado
+document.getElementById("cifrar").addEventListener('click', (evento) => {
+  evento.preventDefault();
+  //guardar valores de los imput
+  let texto = document.getElementById("inputTexto").value;
+  let offset = document.getElementById("inputOffset").value;
+  //guardar en resultado todo el contenido de cipher
+  let resultadoCodificar = cipher.encode(texto, offset);
+  //reemplazar mi parrafo vacio por resultado
   document.getElementById("textoEntregado").innerHTML = resultadoCodificar;
   //console.log(resultadoCodificar);
-  document.getElementById("parrafoTextoEntregado").style.display="block";
+  document.getElementById("parrafoTextoEntregado").style.display = "block";
 
 })
 
 
 //Descifrando
-  document.getElementById("descifrar").addEventListener('click', (evento) => {
-   evento.preventDefault();
-//guardar valores de los imput
-      let textoD = document.getElementById("inputTexto").value;
-      let offsetD = document.getElementById("inputOffset").value;
-//guardar en resultado todo el contenido de cipher
-      let resultadoDecodificar = cipher.decode(textoD, offsetD);
-//reemplazar mi parrafo vacio por resultado
+document.getElementById("descifrar").addEventListener('click', (evento) => {
+  evento.preventDefault();
+  //guardar valores de los imput
+  let textoD = document.getElementById("inputTexto").value;
+  let offsetD = document.getElementById("inputOffset").value;
+  //guardar en resultado todo el contenido de cipher
+  let resultadoDecodificar = cipher.decode(textoD, offsetD);
+  //reemplazar mi parrafo vacio por resultado
   document.getElementById("textoEntregado").innerHTML = resultadoDecodificar;
   //console.log(resultadoDecodificar);
-  document.getElementById("parrafoTextoEntregado").style.display="block";
+  document.getElementById("parrafoTextoEntregado").style.display = "block";
 
 })
 
 
 //Botón volver al inicio
-  document.getElementById("volver").addEventListener('click', (evento) => {
-   evento.preventDefault();
-  document.getElementById("root").style.display="block";
-  document.getElementById("cesar").style.display="none";
+document.getElementById("volver").addEventListener('click', (evento) => {
+  evento.preventDefault();
+  document.getElementById("root").style.display = "block";
+  document.getElementById("cesar").style.display = "none";
 })
